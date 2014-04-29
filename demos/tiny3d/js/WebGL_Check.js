2e1e86779b2f38e5ef53fc60c9c2357b74701ac1
justinholdstock91 <justinholdstock91@gmail.com>
2014-04-29 09:26:57 -0400

    added in canvas creation gl check

--- demos/tiny3d/js/WebGL_Check.js
+++ demos/tiny3d/js/WebGL_Check.js
@@ -15,14 +15,18 @@ function supportsWebGL() {
   };
   browser.firefox_mac = (ua.indexOf('Mac') > -1) && browser.firefox;
 
+  //create a canvas and check
+  var glContextAvailable = ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )();
+
   //run tests
 
     var passed = false;
     if(browser.ie || mobile) {
-
+      //do specifics to mobile and ie
     } 
     else {
-      passed = true;
+      //check for webgl, if all browsers pass!
+      passed = glContextAvailable;
     }
     return passed;
 
