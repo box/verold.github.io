8e1b379502dfafa7c558ec792b59f0da574176af
justinholdstock91 <justinholdstock91@gmail.com>
2014-05-08 14:02:46 -0400

    added in html and style for verold load div

---
+++ javascripts/veroldLoadHTML.js
@@ -0,0 +1,9 @@
+var Verold_Load_HTML = [
+  '<div id="load-scene">',
+  '  <div id="load-cube"></div>',
+  '  <img id="load-verold" src="http://dev.verold.com/images/verold-logo-tex.png"/>',
+  '  <div id="load-progress">',
+  '    <a id="load-text">Loading 3D Experience...</a>',
+  '  </div>',
+  '</div>'
+].join('\n');
