d88fcc30d1ce7a4a619dc7e875064725cfbd065a
Carlos Sanchez <carlos@verold.com>
2014-05-20 10:06:55 -0400

    updates video plugin

--- demos/e2e/js/jquery.videoBG.js
+++ demos/e2e/js/jquery.videoBG.js
@@ -147,8 +147,6 @@
 			.attr('poster',options.poster)
 			.css('top',0)
 			.css('left',0)
-			.css('right',0)
-			.css('margin','auto')
 			.css('min-width','100%')
 			.css('min-height','100%');
 		
